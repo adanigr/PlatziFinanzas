35dcef1a5bd039346bb2e0815f2fcafb21358249
Adán Reséndiz <adanigr@gmail.com>
2019-01-30 17:59:51 -0600

    Initial Commit

---
+++ PlatziFinanzas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlatziFinanzas
+//
+//  Created by Adan Reséndiz on 1/30/19.
+//  Copyright © 2019 Adan Reséndiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
